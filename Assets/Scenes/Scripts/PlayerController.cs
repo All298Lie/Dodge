af70f11c0284007e5cd37a93b94453cea25dfa06
All298Lie <whdudfo333@naver.com>
2025-09-12 13:28:36 +0900

    PlayerController 수정
    조작 방식 변경

--- Assets/Scenes/Scripts/PlayerController.cs
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -13,7 +13,8 @@ public class PlayerController : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        // 키보드 방향키 방향으로 이동
+        /*
+         * 힘을 가해서 움직이는 방식 - AddForce
         if (Input.GetKey(KeyCode.UpArrow) == true) {
             playerRigidbody.AddForce(0.0f, 0.0f, speed);
         }
@@ -26,9 +27,21 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetKey(KeyCode.LeftArrow) == true) {
             playerRigidbody.AddForce(-speed, 0.0f, 0.0f);
         }
+         */
+
+        // 키(키보드, 조이스틱)로부터 입력받은 수치를 가져와 사용할 속도값으로 변환
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        // 벡터값으로 변환 후 플레이어의 Rigidbody 물리에 적용
+        Vector3 newVelocity = new Vector3(xSpeed, 0.0f, zSpeed);
+        playerRigidbody.velocity = newVelocity;
+
     }
 
-    // 플레이어가 사망할 경우
     public void Die() {
         gameObject.SetActive(false);
     }
